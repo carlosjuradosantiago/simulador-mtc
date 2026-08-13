6dbb48fa0251a09fea6662760bb9e6d90e7b1868
Jhoam Carlos Jurado Santiago <ivan.carlos23@gmail.com>
2026-08-12 20:00:50 -0500

    Gate paid access to DEV environment

--- src/config/remoteEnvironments.js
+++ src/config/remoteEnvironments.js
@@ -3,11 +3,13 @@ const REMOTE_ENVIRONMENTS = {
     apiBaseUrl: 'https://flrvcaizsjhieuvhqkxh.supabase.co/functions/v1/api',
     supabaseUrl: 'https://flrvcaizsjhieuvhqkxh.supabase.co',
     supabasePublishableKey: 'sb_publishable_SP83w7KsaRTm12u5ZcvKBQ_aUnupSkc',
+    fullExamFree: false,
   },
   production: {
     apiBaseUrl: 'https://wazikdsfacrawhphzltn.supabase.co/functions/v1/api',
     supabaseUrl: 'https://wazikdsfacrawhphzltn.supabase.co',
     supabasePublishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhemlrZHNmYWNyYXdocGh6bHRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NTk2OTIsImV4cCI6MjA3NTQzNTY5Mn0.--xoCk-6Xq0qmUYDDuatBTLOl2q1Nxns_85A4xaiDOU',
+    fullExamFree: true,
   },
 };
 
